6a75cfe92af1c73b669871bcc459783b1d71af2b
Keishi Asai <isheik808@gmail.com>
2019-05-03 04:22:01 -0700

    Add models

--- models/thermostat.js
+++ models/thermostat.js
@@ -1,52 +1,25 @@
 const Joi = require('joi');
-//const mongoose = require('mongoose');
-const {model, Schema} = require('mongoose');
+const { model, Schema } = require('mongoose');
 
-const daySchedule = new Schema({
-  array: [],
-  ofBoolean: [Boolean]
-});
-const weekSchedule = new Schema({
-    monday:{
-      type: daySchedule,
-    },
-    tuesday:{
-      type: daySchedule,
-    },
-    wednesday:{
-      type: daySchedule,
-    },
-    thursday:{
-      type: daySchedule,
-    },
-    friday:{
-      type: daySchedule,
-    },
-    saturday:{
-      type: daySchedule,
-    },
-    sunday:{
-      type: daySchedule,
-    }
-});
-
-const thermostat = model('thermostat', new Schema({
-  userId:{
-    type: Schema.ObjectId,
-    required: true
-  },
-  thermostatId:{
+const Thermostat = model('Thermostat', new Schema({
+  thermostatId: {
     type: String,
     required: true,
     minlength: 5,
     maxlength: 50
   },
-  room:{
+  masterDevId: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  roomName: {
     type: String,
     required: true,
     lowercase: true,
     minlength: 1,
-    maxlength: 20
+    maxlength: 50
   },
   status:{
     type: Boolean,
@@ -59,26 +32,66 @@ const thermostat = model('thermostat', new Schema({
   },
   setTemp:{
     type: Number,
-    required: true
+    required: true,
+    default: 20.0
   },
-  schedule:{
-    type: weekSchedule
-  }
+  weekSchedule: {
+    mon: [{type: Number, default: 0.0 }],
+    tue: [{type: Number, default: 0.0 }],
+    wed: [{type: Number, default: 0.0 }],
+    thu: [{type: Number, default: 0.0 }],
+    fri: [{type: Number, default: 0.0 }],
+    sat: [{type: Number, default: 0.0 }],
+    sun: [{type: Number, default: 0.0 }],
+  },
+  authedUsers: [{type: Schema.Types.ObjectId, ref: 'User'}],
+}));
+
+const DayLog = model('DayLog', new Schema({
+  year :{
+    type: Number,
+    min: 1000,
+    max: 9999,
+    default: false
+  },
+  month :{
+    type: Number,
+    min: 1,
+    max: 12,
+    default: false
+  },
+  day :{
+    type: Number,
+    minlength: 1,
+    maxlength: 31,
+    default: false
+  },
+  thermostatId: {
+    type: String,
+    ref: 'Thermostat'
+  },
+  dayTemps: [Number],
+  dayAmbientTemps: [Number],
+  is_on: [Boolean],
+  minsOn: [Number],
+  minsSaved: [Number]
 }));
 
 function validateThermostat(thermostat) {
   const schema = {
-    userId: Joi.string(),
+    authedUsers: Joi.array().items(Joi.object(User)),
     thermostatId: Joi.string().required(),
-    room: Joi.string().required(),
+    masterDevId: Joi.string().required(),
+    roomName: Joi.string().required(),
     status: Joi.boolean(),
     mode: Joi.number(),
     setTemp: Joi.number(),
-    schedule: Joi.weekSchedule()
+    weekSchedule: Joi.array().items(Joi.object({type: Number, default: 0.0 }))
   };
 
   return Joi.validate(thermostat, schema);
 }
 
-exports.thermostat = thermostat;
+exports.Thermostat = Thermostat;
+exports.DayLog = DayLog;
 exports.validate = validateThermostat;
