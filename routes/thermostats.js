af0ccdd41f90444358b26208dfacda66f8411c2a
kttkjl <jinglun.li@alumni.ubc.ca>
2019-05-09 01:40:14 -0700

    get thermostat related to user, and update thermostat schedule implemented, thermostat schema and checks implemented

--- routes/thermostats.js
+++ routes/thermostats.js
@@ -1,4 +1,8 @@
-const { Thermostat, validateSchedule } = require("../models/thermostat");
+const {
+  Thermostat,
+  validateSchedule,
+  validateThermostat
+} = require("../models/thermostat");
 const auth = require("../middleware/auth");
 //const validateObjectId = require("../middleware/validateObjectId");
 const moment = require("moment");
@@ -19,22 +23,14 @@ router.get("/", [auth], async (req, res) => {
   res.send({ thermostats });
 });
 
-//create a thermostat
-router.post("/", [auth], async (req, res) => {
-  const { error } = validate(req.body);
+/**
+ * Create a new thermostat
+ */
+router.post("/new", [auth], async (req, res) => {
+  const { error } = validateThermostat(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const thermostat = new thermostat({
-    //add thermostat new
-    // title: req.body.title,
-    // genre: {
-    //   _id: genre._id,
-    //   name: genre.name
-    // },
-    // numberInStock: req.body.numberInStock,
-    // dailyRentalRate: req.body.dailyRentalRate,
-    // publishDate: moment().toJSON()
-  });
+  const thermostat = new thermostat({});
   await thermostat.save();
 
   res.send(thermostat);
@@ -42,31 +38,45 @@ router.post("/", [auth], async (req, res) => {
 
 /**
  * Update thermostat schedule
+ * {
+	"weekSchedule" : {
+		"mon" : [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+		"tue" : [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+		"wed" : [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+		"thu" : [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+		"fri" : [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],
+		"sat" : [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+		"sun" : [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7]
+	},
+	"thermostatId" : "pre-ree",
+	"masterDevId" : "ree"
+}
+
  */
 router.put("/schedule", [auth], async (req, res) => {
-  const { error } = validateSchedule(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const thermostat = await thermostat.findByIdAndUpdate(
-    req.params.id,
-    // {
-    //   manual: req.body.title,
-    //   ther: {
-    //     _id: genre._id,
-    //     name: genre.name
-    //   },
-    //   numberInStock: req.body.numberInStock,
-    //   dailyRentalRate: req.body.dailyRentalRate
-    // },
-    { new: true }
-  );
-
-  if (!thermostat)
-    return res
-      .status(404)
-      .send("The thermostat with the given ID was not found.");
-
-  res.send(thermostat);
+  const { error } = validateSchedule(req.body.weekSchedule);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  }
+  let { thermostatId, masterDevId, weekSchedule } = req.body;
+  // Build query params
+  let query = { thermostatId, masterDevId };
+  let update = {
+    $set: {
+      weekSchedule
+    }
+  };
+  let options = { new: true }; // Returns the updated document instead
+  try {
+    const thermostat = await Thermostat.findOneAndUpdate(
+      query,
+      update,
+      options
+    );
+    res.status(200).send(thermostat);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 //delete a thermostat
