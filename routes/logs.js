20f06c4088c8d7153c9070f772b2c818665077e9
Keishi Asai <isheik808@gmail.com>
2019-05-10 15:28:35 -0700

    Added daylog endpoint

--- routes/logs.js
+++ routes/logs.js
@@ -5,6 +5,32 @@ const express = require("express");
 const _ = require("lodash");
 const router = express.Router();
 
+// Get a daylog
+router.get("/day", [auth], async (req, res) => {
+  const masterId = req.query.master_id;
+  const thermostatId = req.query.thermostat_id;
+  const year = req.query.year;
+  const month = req.query.month;
+  const day = req.query.day;
+
+  if (!(masterId && thermostatId && year && month && day)) {
+    return res.status(404).send("Missing parameters.");
+  }
+
+  const dayLog = await DayLog.findOne({
+    thermostatId: thermostatId,
+    year: year,
+    month: month,
+    day: day
+  });
+
+  if (!dayLog) {
+    return res.status(404).send("No record found.");
+  }
+
+  return res.status(200).send(dayLog);
+});
+
 // Add a daylog
 router.post("/day", async (req, res) => {
   // console.log("request get: ", req.body);
